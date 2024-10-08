613519bbd562e2f321f2f4d05613ad06c06bf7b9
yueherngtang <ytan0257@student.monash.edu>
2024-10-08 13:51:30 +0800

    updated back for w10hw

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -1,12 +1,27 @@
 var vg_1 = "sample.vg.json";
 var vg_2 = "bubble.vg.json";
+var vg_3 = "bar_chart_expenditure_group.vg.json";
+var vg_4 = "bump_chart_expenditure_group.vg.json";
+var vg_5 = "line_scrub.vg.json";
 
-vegaEmbed("#map", vg_1).then(function(result) {
+vegaEmbed("#map", vg_1, { actions: false } ).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
 
-vegaEmbed("#bubble", vg_2).then(function(result) {
+vegaEmbed("#bubble", vg_2, { actions: false }).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
     console.log("Bubble chart successfully embedded!");
+}).catch(console.error);
+
+vegaEmbed("#bar_expenditure", vg_3, { actions: false }).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+vegaEmbed("#bump", vg_4, { actions: false }).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+vegaEmbed("#line_scrub", vg_5, { actions: false }).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
 
